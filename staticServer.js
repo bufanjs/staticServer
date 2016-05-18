394510c22072adddda20229717ee9f3c8f373662
bufan <bufanjs@gmail.com>
2016-05-18 16:10:17 +0800

    add static

--- staticServer.js
+++ staticServer.js
@@ -1,9 +1,17 @@
 var http = require('http');
+var fs = require('fs');
 var server = http.createServer();
 
-server.on('request',function(request,response){
-	response.write('hello world');
-	response.end();
+server.on('request', function(request, response) {
+    fs.readFile(__dirname+"/static/index.html", function(err,data) {
+    	if(err){
+    		console.log(err);
+    		return;
+    	}
+        response.write(data);
+        response.end();
+    });
 });
 
 server.listen(3000);
+console.log("run port 3000");
